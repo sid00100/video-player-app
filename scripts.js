1b4d83c8164a5d4704ef90dd2b2d04c6ab04873c
sid00100 <otasiddhant@gmail.com>
2022-03-25 11:52:38 +0530

    progressbar feature add

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // declarations
-
+let mouseD = false;
 const player = document.querySelector('.player')
 const video = player.querySelector('.viewer')
 const progress = player.querySelector('.progress')
@@ -7,7 +7,6 @@ const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
 const skipBtn = player.querySelectorAll('[data-skip]')
 const range = player.querySelectorAll('.player__slider')
-console.log(video);
 // functions
 
 function togglePlay() {
@@ -21,7 +20,6 @@ function updateBtn() {
     //     toggle.textContent = '⏸︎'
     // } else toggle.textContent = '▶'
     const icon = video.paused ? '▶' : '⏸︎'
-    console.log(icon);
     toggle.textContent = icon
 }
 
@@ -33,11 +31,31 @@ function rangeUpdate() {
     video[this.name] = this.value
 }
 
+function handleUpdate() {
+    const percent = (video.currentTime / video.duration) * 100
+    // console.log(percent);
+    progressBar.style.flexBasis = `${percent}%`
+}
+
+function scrub(e) {
+    const position = video.duration * (e.offsetX / progress.offsetWidth)
+    video.currentTime = position
+}
+
 // event listeners
 toggle.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
 video.addEventListener('pause', updateBtn)
 video.addEventListener('play', updateBtn)
+video.addEventListener('timeupdate', handleUpdate)
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousemove', ()=>{
+    if (mouseD) {
+        scrub
+    }
+})
+progress.addEventListener('mousedown', () => mouseD = true)
+progress.addEventListener('mouseup', () => mouseD = false)
 skipBtn.forEach(btn => {
     btn.addEventListener('click', skipVid)
 })
